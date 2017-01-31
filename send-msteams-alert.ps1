df3222bfe8227ba2e23f1a33586d636f610fe891
nightred <nightred@gmail.com>
2017-01-31 16:21:14 -0600

    Initial Commit

---
+++ send-msteams-alert.ps1
@@ -0,0 +1,53 @@
+﻿# Powershell MS Teams alert template
+# 
+# This is a template to demonstrate the ability to send notices to 
+# Microsoft Teams from PowerShell
+# 
+# Parse the command variables
+param (
+    [string]$message = "This is a test of the alert system",
+    [string]$title = "Test Message",
+    [string]$status = "Cleared",
+    [string]$monitor = "Testing - monitor",
+    [string]$location = "Downtown",
+    [string]$system = "labsys01"
+)
+
+# Edit the URI with the webhook url that was provided
+$uri = 'https://outlook.office365.com/webhook/MISSING';
+
+# Build the message Body
+$body = ConvertTo-Json -Depth 4 @{
+    title = $title
+    text = ' '
+    sections = @(
+        @{
+            activityText = $message
+        },
+        @{
+            facts = @(
+                @{
+                    name = 'Status'
+                    value = $status
+                },
+                @{
+                    name = 'System Name'
+                    value = $system
+                },
+                @{
+                    name = 'Monitor'
+                    value = $monitor
+                },
+                @{
+                    name = 'Location'
+                    value = $location
+                }
+            )
+        }
+    )
+}
+
+#Send the message to MS Teams
+Invoke-RestMethod -uri $uri -Method Post -body $body -ContentType 'application/json';
+
+Write-Output "INFO - Message has been sent.";
